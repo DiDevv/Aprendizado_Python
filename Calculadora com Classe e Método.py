216d52049990ec7ebe581f72bf0ea1bb11ccc20d
Diogo <101913207+DiDevv@users.noreply.github.com>
2023-01-05 09:53:09 -0300

    Update Calculadora com Classe e Método.py

--- Calculadora com Classe e Método.py
+++ Calculadora com Classe e Método.py
@@ -1,3 +1,5 @@
+#Utilizando init para instanciar
+
 class Calculadora:
   def __init__(self, num1, num2):
     self.a = num1
@@ -20,3 +22,25 @@ print(calculadora.soma())
 print(calculadora.sub())
 print(calculadora.mult())
 print(calculadora.div())
+
+#sem utilizar o init
+
+class Calculadora:
+  
+  def soma(self, a, b):
+    return a + b
+
+  def sub(self, a, b):
+    return a - b
+
+  def mult(self, a, b):
+    return a * b
+
+  def div(self, a, b):
+    return a / b
+
+calculadora = Calculadora()
+print(calculadora.soma(1, 2))
+print(calculadora.sub(2, 3))
+print(calculadora.mult(4, 5))
+print(calculadora.div(3, 2))
