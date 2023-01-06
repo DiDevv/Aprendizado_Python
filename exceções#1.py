9ffb48c81b56d4b464eef05830ac10204dfa55a6
Diogo <101913207+DiDevv@users.noreply.github.com>
2023-01-06 15:12:45 -0300

    Create exceções#1.py

---
+++ exceções#1.py
@@ -0,0 +1,13 @@
+#lista = [1, 10]
+
+try:
+    #divisao = 10/0
+    #numero = lista[3]
+    
+except Exception as ex:
+    print('Erro desconhecido. Erro: {}'.format(ex))
+#except ZeroDivisionError:
+    print('Não é possível realizar uma divisão por Zero')
+#except IndexError:
+    print('Erro de Index!')
+
